85a9e1add0889cfa9d1c30f33f9845cc93fe846d
wahyucyber <m.nur.wahyu1974@gmail.com>
2022-07-30 13:49:40 +0700

    Add model and migration task

--- app/Models/Employee.php
+++ app/Models/Employee.php
@@ -10,4 +10,9 @@ class Employee extends Model
     use HasFactory;
 
     protected $fillable = ["name", "gender", "position", "phone", "address", "email"];
+
+    public function tasks()
+    {
+        return $this->hasMany(Task::class);
+    }
 }
